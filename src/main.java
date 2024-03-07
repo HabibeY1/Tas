884e9d674652fa1e51a73641ff80bea3f2fbf935
HabibeY1 <habibeyavuz13@gmail.com>
2024-03-07 11:41:28 +0100

    Initial commit

---
+++ src/main.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+public class main {
+    public static void main (String[]args){
+        Scanner my0bj = new Scanner(System.in);
+        System.out.println("Enter username");
+
+        String username = my0bj.nextLine();
+        System.out.println("Username is:" + username);
+    }
+}
